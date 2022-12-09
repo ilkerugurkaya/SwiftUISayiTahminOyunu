a5e7e9467befff5a9795a14665e51139b9451c83
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-09 09:34:04 +0300

    Initial Commit

---
+++ SayiTahminOyunu/SayiTahminOyunuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayiTahminOyunuApp.swift
+//  SayiTahminOyunu
+//
+//  Created by İlker Kaya on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SayiTahminOyunuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
